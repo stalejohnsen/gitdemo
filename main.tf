59b5c26b306c281e494c3a12edbda116a5c473ea
Ståle Johnsen <stale.johnsen@outlook.com>
2022-06-22 13:43:09 +0200

    En TF ressursgruppe

---
+++ main.tf
@@ -0,0 +1,21 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.10.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "RG-lab1"
+  location = "Norway East"
+}
